286d4c433aeb28ab0271bf2bb2a663679d992757
sjh0120 <simjh980216@naver.com>
2022-05-24 09:05:36 +0900

    2022-05-24 Nodejs day start

--- node20/ex25.js
+++ node20/ex25.js
@@ -1,16 +1,17 @@
 var express=require('express');
 var app=express();
 
-app.use(express.static('static'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app
+    .use(express.static('static'))
+    .use(express.json())
+    .use(express.urlencoded({ extended: true }));
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('/',function(req,res){
     res.setHeader('content-type','text/html; charset=utf-8');
-    res.render('index', { title: 'Hey', message: 'Hello there!'});
+    res.render('index.ejs', { title: 'Hey', message: 'Hello there!'});
 });
 
 app.post('/add.html',function(req,res){
